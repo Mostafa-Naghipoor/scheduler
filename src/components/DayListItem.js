c41085fbad3187796e505089341eb9e20a15caa5
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2023-01-12 21:21:38 -0800

    adding Appointment and related components and stories

---
+++ src/components/DayListItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export d
+efault function DayListItem(props) {
+  return (
+    <li>
+      <h2 className="text--regular">Day Name</h2> 
+      <h3 className="text--light">X spots remaining</h3>
+    </li>
+  );
+}
