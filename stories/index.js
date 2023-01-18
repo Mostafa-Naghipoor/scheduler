806ef44d95917f4146ae33b8ddbc31a39a685531
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-01-17 16:09:10 -0800

    InterviewerListItem stories

--- stories/index.js
+++ stories/index.js
@@ -103,3 +103,38 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+  
+  //////////InterviewerListItem
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
