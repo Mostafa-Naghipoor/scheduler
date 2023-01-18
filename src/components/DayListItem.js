3f6c9d48f60182b279653ceb5bdb6761cc30d2cb
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-01-17 19:34:54 -0800

    add Appointment and related components and stories

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,31 @@
 import React from "react";
+import 'components/DayListItem.scss'
+import classNames from 'classnames';
 
-export d
-efault function DayListItem(props) {
+export default function DayListItem(props) {
+const dayClass = classNames("day-list__item", {
+  "day-list__item--selected": props.selected,
+  "day-list__item--full": props.spots === 0
+});
+
+const formatSpots = function(props) {
+  
+  if (props.spots === 0) {
+    return 'no spots remaining';
+  } 
+  if (props.spots === 1) {
+    return '1 spot remaining';
+  } 
+  if (props.spots > 1) {
+    return `${props.spots} spots remaining`;
+  }
+  
+
+}
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
   );
 }
