ae223408ee7108cd808788ba604339a0c1fcd40d
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-01-17 16:47:38 -0800

    add getInerview tests

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -67,3 +67,24 @@ test("getAppointmentsForDay returns an array", () => {
     const result = getAppointmentsForDay(state, "Wednesday");
     expect(result.length).toEqual(0);
   });
+  
+//getInterview tests
+
+test("getInterview returns an object with the interviewer data", () => {
+    const result = getInterview(state, state.appointments["3"].interview);
+    expect(result).toEqual(
+      expect.objectContaining({
+        student: expect.any(String),
+        interviewer: expect.objectContaining({
+          id: expect.any(Number),
+          name: expect.any(String),
+          avatar: expect.any(String)
+        })
+      })
+    );
+  });
+  
+  test("getInterview returns null if no interview is booked", () => {
+    const result = getInterview(state, state.appointments["2"].interview);
+    expect(result).toBeNull();
+  });
