bd66c82a06b91c43297ab7dc0bc746268eb7ccab
Mostafa Naghipoor <mostafa.naghipoor@hotmail.com>
2023-01-18 23:23:24 -0800

    add const interviewer

---
+++ src/components/__tests__/Form.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import {
+  render,
+  cleanup,
+  fireEvent,
+  getByTestId,
+} from "@testing-library/react";
+
+import Form from "components/Appointment/Form";
+
+afterEach(cleanup);
+
+describe("Form", () => {
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png",
+    },
+  ];
+}
